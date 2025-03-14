386a9d8922cfe99f357257b9a68438f4625a8db8
rmatsuda21 <reo.matsuda@gmail.com>
2025-03-14 13:53:49 -0700

    test wouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
 import { useState } from "react";
 
 import styles from "./App.module.scss";
+import { Route, Switch } from "wouter";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className={styles.root}>
-      <h1>Vite + React</h1>
+    <>
+      <div className={styles.root}>
+        <h1>Vite + React</h1>
 
-      <button onClick={() => setCount((count) => count + 1)}>
-        count is {count}
-      </button>
-    </div>
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+
+      <Switch>
+        <Route path="/test" component={() => <div>Test</div>} />
+      </Switch>
+    </>
   );
 }
 
