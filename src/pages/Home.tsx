af82938fb129bd85e698b011e1da73ae8ba8ca66
rmatsuda21 <reo.matsuda@gmail.com>
2025-03-15 11:45:10 -0700

    update homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,22 @@
-import { Container } from "@radix-ui/themes";
+import { Container, Em, Flex, Heading, Text } from "@radix-ui/themes";
 
 import styles from "./Home.module.scss";
 
 export const Home = () => {
   return (
     <Container className={styles.root}>
-      <h1>Home</h1>
-      <p>Welcome to the home page!</p>
+      <Heading as="h1" size="9">
+        Welcome 👋
+      </Heading>
+      <Flex direction="column" gap="2">
+        <Text as="p">
+          Soon to be page for all of your smash graphic generator tools
+        </Text>
+
+        <Text as="p">
+          <Em>Check back for updates~</Em>
+        </Text>
+      </Flex>
     </Container>
   );
 };
--- src/pages/Ranker.tsx
+++ src/pages/Ranker.tsx
@@ -1,10 +1,19 @@
 import { Container } from "@radix-ui/themes";
+import { useEffect, useRef } from "react";
 
 export const Ranker = () => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {}, []);
+
   return (
     <Container>
       <h1>Ranker</h1>
       <p>Welcome to the ranker page!</p>
+
+      <Container>
+        <canvas ref={canvasRef}></canvas>
+      </Container>
     </Container>
   );
 };
