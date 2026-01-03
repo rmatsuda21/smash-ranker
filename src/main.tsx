6fdeae067c0b492be5810faf4b20ed52bbe9611f
rmatsuda21 <reo.matsuda@gmail.com>
2026-01-03 17:44:24 -0500

    add vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,17 @@
-import { waitForServiceWorker } from "@/utils/waitForServiceWorker";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { inject } from "@vercel/analytics";
 
 import "@/theme.scss";
 import "@/reset.css";
 import "@/index.css";
 
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import { waitForServiceWorker } from "@/utils/waitForServiceWorker";
 
 import App from "@/App";
 
 waitForServiceWorker().then(() => {
+  inject();
   createRoot(document.getElementById("root")!).render(
     <StrictMode>
       <App />
